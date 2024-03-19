26097c662d5ab13f1bcd4533bbfcdaadeb85c447
FerbaDev <elferbarron1@gmail.com>
2024-03-19 11:26:39 -0300

    se configura handlebars

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Práctica integradora : Mongoose
+# Práctica integradora 1: Mongoose - Clase 8 parte II
+
+## Pinterest que almacena usuarios en Mongo DB
 
 empieza con la iniciacion ----------- 02:46
 
-dejé en 02:50
+dejé en 03:02
 
 
 () => {}
+
+`string text`
---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+const PUERTO = 8080;
+import exphbs from "express-handlebars";
+import multer from "multer";
+import "../src/database.js";
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("./src/public"));
+//configuramos handlebars en 3 lineas
+app.engine("handlebars", exphbs.engine()); //le digo a express que cuando encuentre un archivo con la ext handlebars lo tiene que renderizar con el motor de plantillas
+app.set("view engine", "handlebars"); //aca le decimos que el motor es handlebars
+app.set("views", "./src/views"); //aca le pasamos la ruta para que los encuentre
+//Rutas
+app.get("/", (req, res) => {
+    res.render("home")
+})
+//listen
+app.listen(PUERTO, () => {
+    console.log(`Conectado a http://localhost:${PUERTO}`);
+})
